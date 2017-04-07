90fd748c5b388f98d59c220a7f3250962cbd06e1
stevestrong <sstrong@apprenticesystems.com>
2017-04-07 16:24:07 -0400

    extension class

---
+++ OITAppRoles/OITCoreExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using OITAppRoles.Data;
+using OITAppRoles.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OITAppRoles.Services;
+
+namespace OITAppRoles
+{
+    public static class OITCoreExtensions
+    {
+        public static IServiceCollection AddOITIdenitity(this IServiceCollection serviceCollection, IConfigurationRoot Configuration, string ConnectionName)
+        {
+            serviceCollection.AddDbContext<OITApplicationDbContext>(options =>
+            {
+                var connect = Configuration.GetConnectionString(ConnectionName);
+                options.UseSqlServer(connect);
+            });
+
+            serviceCollection.AddIdentity<OITApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<OITApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+
+            serviceCollection.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            });
+
+            // Add application services.
+            serviceCollection.AddTransient<IEmailSender, AuthMessageSender>();
+            serviceCollection.AddTransient<ISmsSender, AuthMessageSender>();
+
+            return serviceCollection;
+        }
+
+        public static IApplicationBuilder UseOITIdenitity(this IApplicationBuilder app)
+        {
+            app.UseIdentity();
+            return app;
+        }
+    }
+}
--- OITAppRoles/Startup.cs
+++ OITAppRoles/Startup.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using OITAppRoles.Data;
-using OITAppRoles.Models;
 using OITAppRoles.Services;
 
 namespace OITAppRoles
@@ -40,19 +32,12 @@ namespace OITAppRoles
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddDbContext<OITApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("OITAppRoles")));
-
-            services.AddIdentity<OITApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<OITApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            services.AddOITIdenitity(Configuration, "AppRolestest");
 
             services.AddMvc();
-
-            // Add application services.
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -73,7 +58,7 @@ namespace OITAppRoles
 
             app.UseStaticFiles();
 
-            app.UseIdentity();
+            app.UseOITIdenitity();
 
             // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
 
